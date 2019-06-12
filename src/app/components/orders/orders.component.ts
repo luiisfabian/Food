c0930846d8b5c5b327bd0150635fe2f1233f282a
Administrator <luisfa-bi9@hotmail.com>
2019-06-11 23:05:54 -0500

    agregando aañadir elementos de compra y quitandolos

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -7,9 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
   appname : String = 'Best Food!';
-  constructor() { }
+  totalPrice = 0;
+  arrayTempProducts = [];
+
+  constructor() {}
+   products = [
+     {
+    name: 'fruit',
+    price: 13,
+  },
+  {
+    name: 'salad',
+    price: 2213
+  },
+  {
+    name: 'potato',
+    price: 33
+  },
+  {
+    name: 'orange',
+    price: 78
+  },
+  {
+    name: 'watermelon',
+    price: 2
+  },
+  {
+    name: 'mango',
+    price: 22
+  },
+
+
+
+
+]
 
   ngOnInit() {
   }
+  addProduct(product) {
+    console.log(product);
+    this.totalPrice = (this.totalPrice * product.price);
+    this.arrayTempProducts.push(product.name);
+  }
+
+  deleteorderTemp = (order) =>{
+    let index = this.arrayTempProducts.indexOf(order);
+    if(index > -1)
+      this.arrayTempProducts.splice(index, 1);
+
+  }
+
 
 }
